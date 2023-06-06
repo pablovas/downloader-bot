020926c35a96b0aed7d720eb4625a363bc80627a
pinterpol <pablo71042001@gmail.com>
2023-06-06 11:01:03 -0300

    switched puppeteer to playwright

--- comandos/micro.js
+++ comandos/micro.js
@@ -1,24 +1,25 @@
-const puppeteer = require('puppeteer');
+const { chromium } = require('playwright');
 
 async function scrapeWebsite() {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto('https://www.furg.br/horarios-do-onibus-interno');
-    await page.waitForSelector('tbody');
-    const tabelaElement = await page.$('tbody');
-    const screenshot = await tabelaElement.screenshot({ encoding: 'binary' });
-  
-    await browser.close();
-  
-    return screenshot;
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://www.furg.br/horarios-do-onibus-interno');
+  await page.waitForSelector('tbody');
+  const tabelaElement = await page.$('tbody');
+  const screenshot = await tabelaElement.screenshot({ fullPage: true });
+
+  await browser.close();
+
+  return screenshot;
 }
 
 module.exports = async (ctx) => {
-    try {
-      const screenshot = await scrapeWebsite();
-      await ctx.replyWithPhoto({ source: screenshot });
-    } catch (error) {
-      console.error('Ocorreu um erro durante o web scraping:', error);
-      ctx.reply('Desculpe, ocorreu um erro durante o web scraping.');
-    }
+  try {
+    const screenshot = await scrapeWebsite();
+    await ctx.replyWithPhoto({ source: screenshot });
+  } catch (error) {
+    console.error('Ocorreu um erro durante o web scraping:', error);
+    ctx.reply('Desculpe, ocorreu um erro durante o web scraping.');
+  }
 };
