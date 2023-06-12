b803959eb905c6d87772329eb39b9091590e37bd
pinterpol <pablo71042001@gmail.com>
2023-06-12 16:25:40 -0300

    config miscs

--- config.js
+++ config.js
@@ -3,12 +3,9 @@ module.exports = {
     logInteraction: function(ctx) {
       const userId = ctx.from.id;
       const username = ctx.from.username;
-      const interaction = ctx.update.message.text;
       const timestamp = new Date().toLocaleString();
-  
       console.log(`ID do usuário: ${userId}`);
       console.log(`Username do usuário: ${username}`);
-      console.log(`Interação: ${interaction}`);
       console.log(`Horário: ${timestamp}`);
     }
   };
