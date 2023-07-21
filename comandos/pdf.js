3baf6da7946e8c392a794672a687e2eb5b946912
pinterpol <pablo71042001@gmail.com>
2023-07-21 14:36:55 -0300

    Added: caption message

--- comandos/pdf.js
+++ comandos/pdf.js
@@ -6,32 +6,42 @@ module.exports = async (ctx) => {
 
     if (!url) {
       ctx.reply('Você precisa fornecer um link para converter em PDF.');
+      ctx.deleteMessage(message.message_id);
       return;
     }
-  
+
     try {
         // Inicializa o browser Puppeteer
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-    
+
         // Navega até a página especificada
-        await page.goto(url, { waitUntil: 'networkidle0' });
+        await page.goto(url, { waitUntil: 'networkidle0', timeout: 10000 });
 
         // Extrai o título da página (nome da aba)
         const title = await page.title();
-  
+
         // Gera o PDF da página
         const pdfBuffer = await page.pdf();
-    
+
         // Fecha o browser Puppeteer
         await browser.close();
-    
+
+        const caption = `[🔗Fonte](${url})`;
+
         // Envia o PDF para o Telegram
-        ctx.replyWithDocument({ source: pdfBuffer, filename: `${title}.pdf` });
+        ctx.replyWithDocument(
+            { source: pdfBuffer, filename: `${title}.pdf` },
+            {
+              caption: `Para ver melhor, considere ler o conteúdo original aqui na [🔗FONTE](${url}).`,
+              parse_mode: 'Markdown',
+            }
+        );
         ctx.deleteMessage(message.message_id);
-   
+
     } catch (error) {
       console.error('Erro ao converter a página em PDF:', error);
       ctx.reply('Ocorreu um erro ao converter a página em PDF.');
+      ctx.deleteMessage(message.message_id);
     }
 }
